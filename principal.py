edb6203b8c5f57ef9bf0931ec861985d15e5fc3a
YulianaOlaya <144632713+YulianaOlaya@users.noreply.github.com>
2024-02-15 20:07:47 -0500

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,23 @@
+"""
+Mi primer programa
+------------------
+
+Ingrese un entero y se imprime de 0 a ese n
+"25"
+
+
+Ejemplo:
+-------
+
+Entrada:
+5
+2
+150
+
+Salida:
+5
+25
+150
+"""
+numero = input("")
+print(numero)
